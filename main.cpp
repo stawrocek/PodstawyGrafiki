3e108c5c1b6e13c8d88054dca3bd31e5fc81595b
Maciej Faron <supremacey@icloud.com>
2018-10-25 19:51:11 +0200

    Added code on projections.

--- main.cpp
+++ main.cpp
@@ -28,7 +28,9 @@ int main(int argc, char* argv[])
 #ifdef APPLE // <- those guys, ugh...
     glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
 #endif
-    GLFWwindow *window = glfwCreateWindow(640, 480, "Hello triangle", NULL, NULL);
+    const auto width = 640u;
+    const auto height = 480u;
+    GLFWwindow *window = glfwCreateWindow(width, height, "Hello triangle", NULL, NULL);
     if (window == NULL)
     {
         std::cerr << "GLFW3::WINDOW::CREATE::FAIL\n";
@@ -65,14 +67,15 @@ int main(int argc, char* argv[])
     glBindVertexArray(vao);
 
     const float vertices[] = {
-           0.0f,  0.5f
+           0.5f,  0.5f
+        , -0.5f,  0.5f
         , -0.5f, -0.5f
         ,  0.5f, -0.5f
     };
     GLuint vbo;
     glGenBuffers(1, &vbo);
     glBindBuffer(GL_ARRAY_BUFFER, vbo);
-    glBufferData(GL_ARRAY_BUFFER, sizeof(float)*6, vertices, GL_STATIC_DRAW);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(float)*8, vertices, GL_STATIC_DRAW);
 
     glVertexAttribPointer(pos2DLoc, 2, GL_FLOAT, GL_FALSE, 0, NULL);
     glEnableVertexAttribArray(pos2DLoc);
@@ -110,9 +113,41 @@ int main(int argc, char* argv[])
         , {0.0f, 0.0f, 0.0f,  1.0f}
     };
     glUniformMatrix4fv(transMatLocation, 1, GL_TRUE, &cTranslation[0][0]);
-
     /* Transformations! */
+
+    /* Projections! */    
+    // local 2 world <==> model
+    auto model = glm::mat4(1.0f);
+    model = glm::rotate(model, glm::radians(-55.0f), glm::vec3(1.0f,0.0f,0.0f));
+    glUniformMatrix4fv(transMatLocation, 1, GL_FALSE, glm::value_ptr(model));
+
+    // world 2 camera <==> view - this transformation is opposite to word origin to camera 
+    auto view = glm::mat4(1.0f);
+    view = glm::translate(view, glm::vec3(0.0f,0.0f,-3.0f));
+    glUniformMatrix4fv(transMatLocation, 1, GL_FALSE, glm::value_ptr(view * model)); // what will happen now?
+
+    // camera 2 clip space 2 NDC <==> projection
+    if (false)
+    {
+        auto projection = glm::perspective(
+            glm::radians(45.0f)
+            , (float)width/(float)height
+            , 0.1f
+            , 100.0f
+        );
+        glUniformMatrix4fv(transMatLocation, 1, GL_FALSE, glm::value_ptr(projection * view * model));
+    }
+    else
+    {
+        // careful with size!!!
+        // https://www.opengl.org/discussion_boards/showthread.php/176144-glm-ortho-problem-SOLVED
+        auto projection = glm::ortho(-1.0f, 1.0f, -1.0f, 1.0f, 0.1f, 100.0f);
+        glUniformMatrix4fv(transMatLocation, 1, GL_FALSE, glm::value_ptr(projection * view * model));
+    }
+    /* Projections! */    
+
     glClearColor(.3f, .3f, .3f, 1.0f);
+    glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
     /*  Insert OpenGL magic here! */
     while(!glfwWindowShouldClose(window))
     {
@@ -120,7 +155,7 @@ int main(int argc, char* argv[])
         glClear(GL_COLOR_BUFFER_BIT);
         program.use();
         glBindVertexArray(vao);
-        glDrawArrays(GL_TRIANGLES, 0, 3);
+        glDrawArrays(GL_TRIANGLE_FAN, 0, 4);
         /*  Insert OpenGL magic here! */
         glfwSwapBuffers(window);
         glfwPollEvents();  // gotta go fast!
